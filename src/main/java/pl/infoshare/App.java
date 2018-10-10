f1e0e344d238a7f7059bf23ea6f81c80f52ce837
Karol Mazanowski <karol.mazanowski92@gmail.com>
2018-10-10 20:04:41 +0200

    10.10.18 w trakcie zajec

--- src/main/java/pl/infoshare/App.java
+++ src/main/java/pl/infoshare/App.java
@@ -1,5 +1,7 @@
 package pl.infoshare;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
+       /*
+       Pierwsze zajecia
+        */
+
         String name = "Karol";
         String lastName = "Mazanowski";
 
@@ -17,7 +23,59 @@ public class App
         System.out.println(name + " " + lastName);
 
 
-        System.out.print(age + "\t" + city);
+        System.out.println(age + "\t" + city);
+
+        /*
+        Rzutowanie
+         */
+
+        double a = (double) 5;
+        System.out.println(a);
+        double b = 2.34;
+        double c = 10.506;
+
+        double result = b/c;
+
+        System.out.println(result);
+        System.out.println("Wynik to: " + b/c);
+
+        /*
+        Instrukcja warunkowa
+         */
+
+        byte age1 = 18;
+
+
+        if (age1 >= 20 && age1 <= 30)
+        {
+            System.out.println("Jestes pelnoletni");
+        }
+        else if (age1 == 26 || age1 == 28)
+        {
+            System.out.println("Masz 26 lub 28 lat");
+        }
+        else{
+            System.out.println("Masz ponizej 20 lat");
+        }
+
+        int agePrim = 20;
+
+        switch (agePrim){
+            case 20:{
+                System.out.println("Wiek jest rowny 20");
+                break;
+            }
+            case 30:{
+                System.out.println("Wiek jest rowny 30");
+                break;
+
+            }
+            default:{
+                System.out.println("Wiek jest inny");
+                break;
+            }
 
     }
+    }
+
 }
