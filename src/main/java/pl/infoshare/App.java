4b94d2af20fb3d87761271c8603a91c3e1bb8d24
Karol Mazanowski <karol.mazanowski92@gmail.com>
2018-10-10 17:31:01 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,23 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "Karol";
+        String lastName = "Mazanowski";
+
+        byte age = 26;
+        String city = "Rypin";
+
+        System.out.println(name + " " + lastName);
+
+
+        System.out.print(age + "\t" + city);
+
+    }
+}
